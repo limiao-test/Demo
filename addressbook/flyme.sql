a52d4ba781dd50c0bf6fa06d3d53ca23adb2f3fc
limiao-test <limiaoaini@vip.qq.com>
2016-12-21 09:50:37 +0800

    Update flyme.sql

--- addressbook/flyme.sql
+++ addressbook/flyme.sql
@@ -1,8 +1,9 @@
 create table flyme(
-  list_ID int,
-  flyme_grop varchar(32)
+  flyme_ID int,--flyme ID
+  flyme_grop varchar(32)--分组
 );
 
+--添加数据
 insert into flyme(list_ID,flyme_grop) values (1,'family');
 insert into flyme(list_ID,flyme_grop) values (2,'friend');
 insert into flyme(list_ID,flyme_grop) values (3,'family');
